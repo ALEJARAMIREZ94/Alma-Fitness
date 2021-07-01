7f3a951c1844e06c8595b2704cffdc4797bd6b3c
Alejandra Ramirez <arabril26@gmail.com>
2021-07-01 15:14:25 -0300

    cambie el form

---
+++ scss/enviar.php
@@ -0,0 +1,26 @@
+
+<?php
+$nombre = $_POST['nombre'];
+$mail = $_POST['email'];
+$apellidos = $_POST['apellido'];
+$correo = $_POST['correo'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'arabril26@gmail.com';
+$asunto = 'Mensaje de mi sitio web';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:index.html"); /*a donde envia al usuario luego de completar el form*/
+?>
+enviar2.php
+Mostrando enviar2.php
